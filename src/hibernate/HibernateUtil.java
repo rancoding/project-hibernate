9f3ce3ecbae30127254c6a278c97f81bb852e036
Gustavo Vieira <gustavo.vieira.tgp1@gmail.com>
2018-06-12 12:32:51 +0100

    Add .gitignore and Rename HibernateUtil

--- src/hibernate/HibernateUtil.java
+++ src/hibernate/HibernateUtil.java
@@ -14,7 +14,7 @@ import org.hibernate.SessionFactory;
  *
  * @author Rafael
  */
-public class NewHibernateUtil {
+public class HibernateUtil {
 
     private static final SessionFactory sessionFactory;
     
